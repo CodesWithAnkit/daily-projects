b38aab2f136ef01b39cdd64792dac6c2c7aeb5fd
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-31 18:10:23 +0530

    movie seat booking

---
+++ movie-seat-booking/script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = parseInt(movieSelect.value);
+
+// update toatal and count
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatCount = selectedSeats.length;
+  count.innerText = selectedSeatCount;
+  total.innerText = selectedSeatCount * ticketPrice;
+};
+
+// Movie tickect event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = parseInt(e.target.value);
+  updateSelectedCount();
+});
+
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
