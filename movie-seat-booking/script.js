33ebc7bab7ccd33d3b5c5e6bb93296bfc77cad73
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-02-01 20:24:08 +0530

    A simple video player

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -6,17 +6,50 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = parseInt(movieSelect.value);
 
+//   Movie Index and price
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
 // update toatal and count
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  //   geting index of all seats
+  const seatIndex = [...selectedSeats].map(seat => {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
+
   const selectedSeatCount = selectedSeats.length;
   count.innerText = selectedSeatCount;
   total.innerText = selectedSeatCount * ticketPrice;
 };
 
+// Get data From loacal storeage and populateUi
+const populateUi = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+};
+
+populateUi();
 // Movie tickect event
 movieSelect.addEventListener('change', e => {
   ticketPrice = parseInt(e.target.value);
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
@@ -30,3 +63,6 @@ container.addEventListener('click', e => {
     updateSelectedCount();
   }
 });
+
+// Intial load
+updateSelectedCount();
