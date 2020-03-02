5e5c4181659ff3a19764575e676bfe54da95a0b5
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-03-02 20:24:12 +0530

    full navigation

---
+++ fullscreen-overlay-navigation/app.js
@@ -0,0 +1,9 @@
+const nav = document.getElementById('nav');
+const toggle = document.getElementById('toggle');
+
+const menuToggle = () => {
+  nav.classList.toggle('active');
+  toggle.classList.toggle('active');
+};
+
+// toggle.onclick(menuToggle);
